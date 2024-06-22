4ade6e363081953f8ec826483a6e00f7e5dd000f
오지은 <w2317@e-mirim.hs.kr>
2024-06-22 14:23:38 +0900

    [react] input 컴포넌트 만들기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,19 +6,22 @@ import React from 'react'
 import Home from './pages/home.js'
 import About from './pages/about.js'
 import Counter from './pages/counter.js'
+import Input from './pages/input.js'
 
 function App() {
   return (
     <div className="App">
       <nav>
         <Link to="/home">Home</Link>  |
-        <Link to="/about">About</Link>  |  
-        <Link to="/counter">Counter</Link>
+        <Link to="/about">About</Link>  |
+        <Link to="/counter">Counter</Link>  |
+        <Link to="/input">Input</Link>
       </nav>
       <Routes>
         <Route path="/home" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/counter" element={<Counter />}></Route>
+        <Route paht="/input" element={<Input />}></Route>
       </Routes>
     </div>
   );
